ed26cd38bdbf0ea69d17e13b08c6e50e0b0f9318
Lareta064 <lareta064@gmail.com>
2023-09-10 15:33:50 +0300

    fix input

---
+++ src/js/form.js
@@ -0,0 +1,27 @@
+const formItems = document.querySelectorAll('.form-item');
+if (formItems.length >0){
+	for (item of formItems){
+		const inputEl = item.querySelector('.form-input');
+		const plhEl = item.querySelector('.fake-placeholder');
+		const textEl = item.querySelector('p');
+		if (inputEl.classList.contains('disable')){
+			if (inputEl.value.length>0){
+				plhEl.classList.add('active');
+			}
+		}
+		inputEl.addEventListener('focus',()=>{
+			inputEl.classList.add('valid');
+			plhEl.classList.add('active');
+		});
+		inputEl.addEventListener('blur', () => {
+			
+			if (inputEl.value.length == 0){
+				
+				inputEl.classList.remove('valid');
+				plhEl.classList.remove('active');
+				
+			}
+			
+		});
+	}
+}
