9bf19b30f6f459d7d3e9f031d99f81e1e371d8b0
Lareta064 <lareta064@gmail.com>
2023-10-03 18:54:39 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function (){
 	
-
+	$('.header-slider').owlCarousel({
+		items:1,
+		dots: true,
+		loop: true,
+		smartSpeed: 800,
+		autoplay: true,
+	})
 });
