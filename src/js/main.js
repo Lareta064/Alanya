9a2f05521afa93f1e238468ccfa7ef7c2e4445b1
Lareta064 <lareta064@gmail.com>
2023-10-04 09:20:52 +0300

    call-group

--- src/js/main.js
+++ src/js/main.js
@@ -3,41 +3,41 @@ document.addEventListener("DOMContentLoaded", function (){
 	$('.header-slider').owlCarousel({
 		items:1,
 		dots: true,
-		// loop: true,
+		loop: true,
 		smartSpeed: 800,
-		// autoplay: true,
+		autoplay: true,
 		
 		animateOut: 'fadeOut',
 	});
     /*==== main-menu ========= */
-	$(function () {
-		var Accordion = function (el, multiple) {
-			this.el = el || {};
-			this.multiple = multiple || false;
+	// $(function () {
+	// 	var Accordion = function (el, multiple) {
+	// 		this.el = el || {};
+	// 		this.multiple = multiple || false;
 
-			// Variables
-			var link = this.el.find('.main-menu-link');
-			// Eventos
-			link.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
-		}
+	// 		// Variables
+	// 		var link = this.el.find('.main-menu-link');
+	// 		// Eventos
+	// 		link.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
+	// 	}
 
-		Accordion.prototype.dropdown = function (e) {
-			var $el = e.data.el;
-			$this = $(this),
-				$next = $this.next();
-
-			
-			$next.slideToggle();
-			
-			$this.parent().toggleClass('open');
+	// 	Accordion.prototype.dropdown = function (e) {
+	// 		var $el = e.data.el;
+	// 		$this = $(this),
+	// 			$next = $this.next();
 
 			
-			if (!e.data.multiple) {
-				$el.find('.main-menu__sub').not($next).slideUp().parent().removeClass('open');
-			}
+	// 		$next.slideToggle();
+			
+	// 		$this.parent().toggleClass('open');
 
-		}
+			
+	// 		if (!e.data.multiple) {
+	// 			$el.find('.main-menu__sub').not($next).slideUp().parent().removeClass('open');
+	// 		}
+
+	// 	}
 		
-		let accordion = new Accordion($('#main-menu-list'), false);
-	});
+	// 	let accordion = new Accordion($('#main-menu-list'), false);
+	// });
 });
